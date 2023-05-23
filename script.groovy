87eab0633a8a879d517c69fe9a55f652adc924b2
abhijeetdandekar333 <69498032+abhijeetdandekar333@users.noreply.github.com>
2023-05-23 12:46:30 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,4 +1,10 @@
 def buildApp() {
   echo 'Building the application...'
 }
+def testApp() {
+  echo 'testing the application...'
+}
+def deployApp() {
+  echo 'Deploying the application...'
+}
 return this
